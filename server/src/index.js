9e914634d089a68a2910c05f0649bcf75c3a47ef
nikhil94058 <kumarnikhil94058@gmail.com>
2026-01-09 21:37:27 +0530

    env-example added

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,7 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/invoice_fraud_watch';
-
+console.log(MONGO_URI);
 mongoose
   .connect(MONGO_URI)
   .then(() => {
